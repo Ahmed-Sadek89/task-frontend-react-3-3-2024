d046bdbd811cadf70cedc4175f37f6845b30a1e4
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2024-03-06 00:50:13 +0200

    develop profile.tsx

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { Avatar, Box, Container, Typography } from '@mui/material'
+import { profileContainerStyle, profileImageStyle } from './styles'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <Container>
+      <Box sx={profileContainerStyle}>
+        <Avatar sx={profileImageStyle} >AS</Avatar>
+        <Typography variant='h6'>Ahmed Sadek</Typography>
+        <Typography variant='body2'>https://www.linkedin.com/in/ahmed-sadek-2b27541b9/</Typography>
+      </Box>
+    </Container>
   )
 }
 
---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,11 @@
+import { boxContainer } from "../../assets/globalStyle";
+
+export const profileContainerStyle = {
+    ...boxContainer,
+    justifyContent: 'center',
+    textAlign: "center",
+    alignItems: 'center',
+    padding: { xs: '30px 10px', md: '60px' }
+}
+
+export const profileImageStyle = { bgcolor: 'secondary.dark', width: "200px", height: '200px' }
